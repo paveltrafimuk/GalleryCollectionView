23509e3cfe8ba19a4fdde678ef41166c41f08e0a
Pavel Trafimuk <paveltrafimuk@icloud.com>
2020-02-05 12:05:31 +0300

    updated min version of dependences

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
                  targets: ["GalleryCollectionView"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/paveltrafimuk/View2ViewTransition", from: "1.3.4"),
+        .package(url: "https://github.com/paveltrafimuk/View2ViewTransition", from: "1.4.0"),
     ],
     targets: [
         .target(name: "GalleryCollectionView",
